d35b776ec36915ff07146b1231e44722cc796909
Ankit Kumar <101239757+codeburn12@users.noreply.github.com>
2023-01-04 19:54:08 +0530

    Create classComponent.js

---
+++ classComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+class ClassComponent extends Component{
+    render(){
+        return <h1>Class Component</h1>
+    }
+}
+
+export default ClassComponent;
+
+
+/*
+1. Import react and component class fron react
+2. Make the component    A. extends the component class from react    B. implement render method to return null or HTML
+3. Export that component name
+4. Import in App.js
+5. Write componentName in html tag form in return div in App.js
+*/ 
