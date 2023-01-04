ee56441c0a598bcefab0a582b5ab32d5321d6adf
Ankit Kumar <101239757+codeburn12@users.noreply.github.com>
2023-01-04 19:54:45 +0530

    Create functionalComponent.js

---
+++ functionalComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const functional = () => <h1>Functional Component</h1>
+export default functional             // Can be imported different name component in App.js
+
+// export const functional = () => <h1>Hello Ankit</h1>     // Must have same name imported in App.js
+
+
+
+/*
+TO CREATE COMPONENT
+1. Import react 
+2. Make the component
+3. Export that component name
+4. Import in App.js
+5. Write componentName in html tag form in return div in App.js
+*/
